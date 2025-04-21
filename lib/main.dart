4693707bd6e96ba7b1b9f0160c9a455c24d21720
hamidovdev <muhammadrasulcyber@gmail.com>
2025-04-21 17:24:07 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:homework36/screens/splash.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(const MainApp());
 }
 
-class MainApp extends StatefulWidget {
-  MainApp({super.key});
-
-  @override
-  State<MainApp> createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  int? ind;
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            for (int i = 0; i < 5; i++)
-              GestureDetector(
-                onTap: () {
-                  if(ind == i){
-                    ind = null;
-                  }
-                  else{
-                    ind = i;
-                  }
-                  setState(() {});
-                },
-                child: Container(
-                  width: 20,
-                  height: 20,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(
-                      100,
-                    ),
-                    color:  ind != null && i <= ind! ? Colors.yellow : Colors.blue,
-                  ),
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 5,
-                  ),
-                ),
-              )
-          ],
+        body: Center(
+          child: SplashScreen(),
         ),
       ),
     );
